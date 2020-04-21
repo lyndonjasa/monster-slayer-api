68519bfebebe21bf6e0fbcdf72a9b82f1878efb9
Don Jasa <LyndonJ@magenic.com>
2020-04-21 13:37:55 +0800

    made ES6 compatible

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("asdf");
+import express from "express";
+import cors from "cors";
+import "./mongo/mongoose-setup";
+
+var app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Monster Slayer API");
+});
+
+app.listen(port, () => {
+  console.log("server listening to port: " + port);
+});
---
+++ shared/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  mongoConnection: "mongodb+srv://donjasa:d0nJasa0716@cluster0-t0jef.mongodb.net/monster-slayer"
+};
