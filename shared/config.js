32fd35989e91fe7167d8acf41ba8637c1508ad62
Don Jasa <LyndonJ@magenic.com>
2020-04-29 14:10:13 +0800

    connection to mongo atlast

--- shared/config.js
+++ shared/config.js
@@ -1,6 +1,6 @@
 const configuration = {
-  // mongoConnection: "mongodb+srv://donjasa:d0nJasa0716@cluster0-t0jef.mongodb.net/monster-slayer"
-  mongoConnection: "mongodb://127.0.0.1:27017/monster-slayer",
+  mongoConnection: "mongodb+srv://donjasa:d0nJasa0716@cluster0-t0jef.mongodb.net/monster-slayer"
+  // mongoConnection: "mongodb://127.0.0.1:27017/monster-slayer",
   environment: "Staging"
 };
 
