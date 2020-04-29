d5364589d241531fdbf5cc753e7322ed7a29150b
Don Jasa <LyndonJ@magenic.com>
2020-04-29 14:11:54 +0800

    fixed config

--- shared/config.js
+++ shared/config.js
@@ -1,5 +1,5 @@
 const configuration = {
-  mongoConnection: "mongodb+srv://donjasa:d0nJasa0716@cluster0-t0jef.mongodb.net/monster-slayer"
+  mongoConnection: "mongodb+srv://donjasa:d0nJasa0716@cluster0-t0jef.mongodb.net/monster-slayer",
   // mongoConnection: "mongodb://127.0.0.1:27017/monster-slayer",
   environment: "Staging"
 };
